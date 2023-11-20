e5c9225c1ec8632f6cc6508f6db4bbf24b76546f
Pyatin <saschs.puatin@gmail.com>
2023-11-20 10:31:45 +0300

    f

---
+++ DLL demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using volodyaLibrary;
+
+namespace DLL_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vova = new volodya();
+            vova.SayHello();
+            Console.WriteLine($"5 + 5 = {vova.sum(5, 5)}");
+            Console.WriteLine($"5 - 5 = {vova.minus(5, 5)}");
+            Console.WriteLine($"5 * 5 = {vova.multiply(5, 5)}");
+            Console.WriteLine($"5 ^ 5 = {vova.pow(5, 5)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ volodyaLibrary/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace volodyaLibrary
+{
+    public class volodya
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Привет");
+        }
+        public double sum(double a, double b)
+        {
+            return a + b;
+        }
+        public double minus(double a, double b)
+        {
+            return a - b;
+        }
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double pow(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+}
